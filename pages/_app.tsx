54b7d9c8617ec8fcb6ad09ef34e7601a0503b53e
SOMON SOUM <somon.soum9@gmail.com>
2023-01-18 22:20:11 +0700

    make some change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/main.scss'
 import type { AppProps } from 'next/app'
+import { createContext, useState } from 'react';
+export const AuthContext = createContext<{
+  token?: string,
+  setToken: (token: string) => void
+}>({ token: "", setToken: () => { } })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [token, setToken] = useState('')
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token, setToken
+      }}>
+      <Component {...pageProps} />
+    </AuthContext.Provider>
+  )
 }
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,22 +1,53 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
 import { Button, Container, Form, FormGroup, Input, Label } from "reactstrap";
+import { AuthContext } from "./_app";
 
-const baseURI = 'http://localhost:3001/api/v1'
+const baseURI = 'https://jsonplaceholder.typicode.com'
 
 export const AxiosClient = axios.create({ baseURL: baseURI })
 
 export default function LoginPage() {
+
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const getUsers = async () => {
+    const { data } = await AxiosClient.get('/users')
+    setUsers(data)
+  }
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      getUsers();
+      setLoading(false);
+    }, 1000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
+
+  console.log(users);
+
+
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const context = useContext(AuthContext)
+  context.setToken('4444');
+  // console.log(context.token);
+  // GET, POST, PATCH, PUT, DELETE
+
+
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
     let input = { email, password }
-    AxiosClient.post("/login", input).then((res) => {
-      localStorage.setItem("token", res?.data?.token)
-    }).catch((error) => console.error(error))
+    // AxiosClient.post("/login", input).then((res) => {
+    //   localStorage.setItem("token", res?.data?.token)
+    // }).catch((error) => console.error(error))
   }
+
 
   return (
     <Container className="mt-5">
